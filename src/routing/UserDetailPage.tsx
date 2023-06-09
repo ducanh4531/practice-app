acada63caa0169b5b4bf5b0b57b2c9f8f7537be6
ducanh4531 <ducanhle1794@gmail.com>
2023-06-09 23:22:25 +0800

    nested routes

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,5 +1,13 @@
+// import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
-  return <p>User</p>;
+	// USE THREE HOOKS, CAN GET INFORMATION ABOUT THE CURRENT ROUTE
+	// PATHNAME, QUERY STRING PARAMETERS AND UPDATE QUERY STRING PARAMETERS
+	// const params = useParams();
+	// const location = useLocation();
+	// const [searchParams, setSearchParams] = useSearchParams();
+
+	return <p>User</p>;
 };
 
 export default UserDetailPage;
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import ContactPage from "./ContactPage";
 import HomePage from "./HomePage";
+import Layout from "./Layout";
 import UserDetailPage from "./UserDetailPage";
 import UserListPage from "./UserListPage";
 
 const router = createBrowserRouter([
-	{ path: "/", element: <HomePage /> },
-	{ path: "/users", element: <UserListPage /> },
-	{ path: "/users/:id", element: <UserDetailPage /> },
-	{ path: "/contact", element: <ContactPage /> },
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{ index: true, element: <HomePage /> }, // index: true | path: ''
+			{ path: "users", element: <UserListPage /> },
+			{ path: "users/:id", element: <UserDetailPage /> },
+			{ path: "contact", element: <ContactPage /> },
+		],
+	},
 ]);
 
 export default router;
