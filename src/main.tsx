9e736729eae8c10a9b7d6dece18eaf9f10c30fe0
ducanh4531 <ducanhle1794@gmail.com>
2023-06-09 22:23:41 +0800

    set up routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import App from "./App";
+// import App from "./App";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import router from "./routing/routes";
 
 const queryClient = new QueryClient({
 	// customize default settings globally here
@@ -40,7 +42,9 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<App />
+			{/* ROUTING */}
+			<RouterProvider router={router}></RouterProvider>
+			{/* <App /> */}
 			<ReactQueryDevtools />
 		</QueryClientProvider>
 	</React.StrictMode>
