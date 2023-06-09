d68499a74449d6ca3b4ba5e7ba960acc2e1ca9ed
ducanh4531 <ducanhle1794@gmail.com>
2023-06-09 22:36:19 +0800

    add navigation in ContactPage

--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return (
-    <>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Incidunt, mollitia!
-      </p>
-      <a href="/users">Users</a>
-    </>
-  );
+	return (
+		<>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+				Incidunt, mollitia!
+			</p>
+			{/* use Link component, instead of 'a' element */}
+			{/* can help to avoid fetching an entire webpage from server */}
+			<Link to="/users">Users</Link>
+		</>
+	);
 };
 
 export default HomePage;
